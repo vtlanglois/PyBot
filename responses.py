5def2a9a76df7b5a81dd6c89e7919c1c5ef4bef1
Langlois, Vince Thomas <vtlanglo@iu.edu>
2023-02-04 23:07:37 -0500

    added basic chatbot functionality thru OpenAI api

--- responses.py
+++ responses.py
@@ -4,14 +4,13 @@ import secrets
 
 openai.api_key = secrets.OPEN_AI_KEY
 
-
 def handle_response(message) -> str:
     # Split message into arguments
     args = message.lower().split(" ")
     nargs = len(args)
 
     if args[0] == '!image':
-        prompt = ' '.join(args[1:nargs])
+        # assume the args after the !image command is the image generation prompt
         response = openai.Image.create(
             prompt=prompt,
             n=1,
@@ -19,4 +18,25 @@ def handle_response(message) -> str:
         )
         image_url = response['data'][0]['url']
         return image_url
+
+    if args[0] == '!ask':
+        prompt = convert_args_to_prompt(args[1:nargs])
+        prompt = "Answer the following prompt as simple as possible, with sufficient information. If your response has code, please use Markdown formatting for your code. The prompt is: " + prompt
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt,
+            temperature=0.8,
+            max_tokens=255,
+            top_p=1.0,
+            frequency_penalty=0.5,
+            presence_penalty=0.0
+        )
+
+        answer = response['choices'][0]['text']
+        return answer
     return args
+
+
+#convert argument format to a string prompt
+def convert_args_to_prompt(args) -> str:
+    return ' '.join(args)
