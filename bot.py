daa30dea0372b09d4d380baeb9555e8bd16e31b0
Langlois, Vince Thomas <vtlanglo@iu.edu>
2023-02-04 21:24:41 -0500

    init python discord bot

---
+++ bot.py
@@ -0,0 +1,35 @@
+import discord
+import responses
+import secrets
+
+async def send_message(message, user_message):
+    try:
+        response = responses.handle_response(user_message)
+        await message.channel.send(response)
+    except Exception as e:
+        print(e)
+
+def run_discord_bot():
+    TOKEN = secrets.TOKEN
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+        
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+        if user_message[0] == "!":
+            await send_message(message, user_message)
+
+
+    
+    client.run(TOKEN)
---
+++ responses.py
@@ -0,0 +1,8 @@
+import random
+
+def handle_response(message) -> str:
+    args = message.lower().split(" ")
+    nargs = len(args)
+    return args
+
+    
